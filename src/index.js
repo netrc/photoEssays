60ac97cd61fe72aa9bfb9187e6c7172b4d972a21
richard <ric@netrc.com>
2022-11-29 16:11:14 +0000

    js: div

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const peInit = n => state.name = n
 
 const peUpdate = () => {
     for (div of document.getElementsByTagName("div")) {
-        let pe = element.getAttribute('pe');
+        let pe = div.getAttribute('pe');
 console.log('pe: ',pe)
         if (pe != '') {
             div.style.border = "3px solid orange"
