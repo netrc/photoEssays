502666c3611c8da9f3b11c2ef2dd43e6beda11bf
richard <ric@netrc.com>
2022-11-29 16:17:01 +0000

    testing div update

--- src/index.js
+++ src/index.js
@@ -9,8 +9,9 @@ const peUpdate = () => {
     for (div of document.getElementsByTagName("div")) {
         let pe = div.getAttribute('pe');
 console.log('pe: ',pe)
-        if (pe != '') {
+        if (pe != null) {
             div.style.border = "3px solid orange"
+            div.innerHTML = 'setting inner html - append image: ' + div.getAttribute('pename')
         }
     }
 }
